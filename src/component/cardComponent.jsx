37dc03a377498a0576964fe6d1238624e9f3204d
gabedossa <gabrielvinces2@gmail.com>
2024-03-23 19:02:04 -0300

    Finishing the task

--- src/component/cardComponent.jsx
+++ src/component/cardComponent.jsx
@@ -2,7 +2,7 @@ import "./cardStyle.css"
 function CardComponent() {
   return (
     <div className="card">
-                <div className="container">
+        <div className="container">
                     <div className="content">
                 <div className="text_aling">
                     <p className="title">Join our community</p>
@@ -11,28 +11,31 @@ function CardComponent() {
                     Perfect for any developers who are serious about honing their skills</p>
                 </div>
                 </div>
-        </div>
+            </div>
+        
         <div className="bottom">
             <div className="left">
-                <div className="container">
                     <div className="content">
                         <p className="bottom_title_text">Monthly Subscription</p>
                         <p className="price"> <spam className="attention">$29</spam> per month</p>
                         <p className="justificativa">Full access for less than $1 a day</p>
                         <button className="Sign_Up">Sign Up</button>
                     </div>
-                </div>
-
             </div>
+
             <div className="right">
+            <div className="content">
                 <div className="right_container">
                 <div className="right_content">
                     <p className="bottom_title_text">Why Us</p>
                     <p className="right_justificativa">Tutorials by industry experts Peer & expert code review Coding exercises Access to our GitHub repos Community forum Flashcard decks New videos every week</p>
                 </div>
                 </div>
+                </div>
             </div>
+        
         </div>
+
     </div>
   );
 }
